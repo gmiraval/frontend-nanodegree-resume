e003e06bd9f4b3586e84659a01e89545f005b487
gmiraval <miravallesg@gmail.com>
2016-11-14 16:50:17 -0300

    udacity - JavaScript Basics - Lesson 3 - Data types - 5 of 21

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,29 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+/*  $("#main").append("Guillermo Miravalles"); */
+ 
+/*  var firstname = "Guillermo";
+ var age = 42;
+ 
+ console.log(firstname); */
+ 
+/*  var firstname = "Guillermo";
+ var awesomeThoughts = "I am " + firstname + " and I am AWESOME!";
+ console.log(awesomeThoughts);
+ 
+ var funThoughts = awesomeThoughts.replace("AWESOME!","FUN");
+ $("#main").append(funThoughts); */
+ 
+ 
+ var formattedName = HTMLheaderName.replace("%data%","Guillermo Miravalles");
+ var formattedRole = HTMLheaderRole.replace("%data%","Engineer");
+ 
+ $("#header").append(formattedName);
+ $("#header").append(formattedRole);
+ 
+ 
+ 
+
+ 
